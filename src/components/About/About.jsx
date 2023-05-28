f993b1b9099537d7bc89688383f6489c6af31c0e
amirhossein <amirhosseinhphpam@gmail.com>
2023-05-29 00:34:25 +0330

    add react animations

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-
-import profile from '../../assets/img/profile.png'
+import { TypeAnimation } from 'react-type-animation'
 
 const About = () => {
     return (
         <div className='w-10/12 lg:w-1/2 absolute translate-x-[-50%] translate-y-[-50%] left-[50%] top-[50%]'>
             <div className='w-full flex flex-col items-center justify-center'>
                 <h1 className='text-[25px] sm:text-[35px] text-center'>سلام، من امیرحسین حسین پور هستم</h1>
-                <p className='pt-2 pb-5 text-[20px] text-blue sm:text-[25px]'>فرانت اند دولوپر</p>
+                <p className='pt-2 pb-5 text-[20px] text-blue sm:text-[25px]'>
+                    <TypeAnimation sequence={[
+                        'برنامه نویس فرانت اند',
+                        2000,
+                        'یوتیوبر',
+                        2000,
+                    ]}
+                        wrapper='p'
+                        repeat={Infinity}
+                    />
+                </p>
                 <p className='leading-7 pb-12 text-[15px] sm:text-[16px] text-center'>
                     مسلط به مفاهیم ری اکت
                     توانایی راه اندازی انواع پروژه های تک صفحه ای و چند صفحه ای
